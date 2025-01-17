1cb8f1df568259f387e054ffaaf849b6e2eae9cd
Sandeep Thukral <sandeepthukral@gmail.com>
2025-01-17 16:10:30 +0100

    chore: added secrets file

---
+++ secrets/secrets.ts
@@ -0,0 +1,9 @@
+import {User} from "../models/data.model";
+
+const secretUserAgent = 'Funda Interviewee/1.0.0 (1edea7ee-c572-4da0-b13e-a50ebca8e918)'
+
+const user : User= {
+    emailAddress: 'sandeep.funda@mailgw.com',
+    password: 'e!vxTIt4lhQjU1qm',
+}
+export { secretUserAgent, user };
